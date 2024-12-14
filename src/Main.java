30262c2967d89bcde73947abf94c5b2a54d1ce58
Andri11i <zotov.andrey.2008@gmail.com>
2024-12-14 10:57:02 +0200

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
+
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + ", ");
+            current = current.next;
+        }
+    }
+
+
+    public static ListNode createLinkedList(int[] values) {
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] values = {1, 2, 3, 4, 5};
+        ListNode head = createLinkedList(values);
+
+
+        printList(head);
+
+        System.out.println("\n");
+        ListNode reversedHead = reverseList(head);
+
+
+        printList(reversedHead);
     }
 }
